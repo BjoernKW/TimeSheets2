3233bb125da3761e827abeb9cd9497f57244e4c7
Björn Wilmsmann <bjoern@wilmsmann.de>
2016-01-10 00:21:43 +0100

    Bootstrap theme

--- tools/config.ts
+++ tools/config.ts
@@ -54,7 +54,8 @@ export const NPM_DEPENDENCIES = [
   { src: 'angular2/bundles/router.js', inject: 'libs', dest: LIB_DEST }, // use router.min.js with alpha47
   { src: 'angular2/bundles/http.min.js', inject: 'libs', dest: LIB_DEST },
 
-  { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true, dest: CSS_DEST }
+  { src: 'bootstrap/dist/css/bootstrap.min.css', inject: true, dest: CSS_DEST },
+  { src: 'bootstrap/dist/css/bootstrap-theme.min.css', inject: true, dest: CSS_DEST }
 ];
 
 // Declare local files that needs to be injected
